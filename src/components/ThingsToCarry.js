ac145160951485ff4c77dd2a131c049f360b01d8
Rishi N Desai <rishindesai@yahoo.com>
2022-04-15 02:44:00 +0530

    finalized carousel component

--- src/components/ThingsToCarry.js
+++ src/components/ThingsToCarry.js
@@ -1,47 +1,52 @@
 import Carousel from "react-multi-carousel";
+import { getImagesOfThingsToCarry } from "../utils/thingsToCarry";
 import "react-multi-carousel/lib/styles.css";
 import "./styles/ThingsToCarry.css";
 
 const ThingsToCarry = ({ weatherType }) => {
-  return (
-    <div className="center-content">
-      <div className="carousel-container">
-        <h2>Things to Carry</h2>
-        <Carousel
-          draggable={false}
-          // partialVisible={true}
-          itemClass="image-item"
-          responsive={{
-            desktop: {
-              breakpoint: { max: 3000, min: 1024 },
-              items: 3,
-              // paritialVisibilityGutter: 60
-            },
-            tablet: {
-              breakpoint: { max: 1024, min: 600 },
-              items: 2,
-              // paritialVisibilityGutter: 50
-            },
-            mobile: {
-              breakpoint: { max: 600, min: 0 },
-              items: 1,
-              // paritialVisibilityGutter: 30
-            },
-          }}
-        >
-          {[
-            "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            "https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-            "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            "https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-          ].map((image) => {
-            return <img style={{ width: "100%", height: "100%" }} src={image} alt="" />;
-          })}
-        </Carousel>
+  const imagesOfThingsToCarry = getImagesOfThingsToCarry(weatherType);
+
+  if (imagesOfThingsToCarry.length) {
+    return (
+      <div className="center-content">
+        <div className="carousel-container">
+          <h2>Things to Carry</h2>
+          <Carousel
+            draggable={false}
+            itemClass="image-item"
+            responsive={{
+              desktop: {
+                breakpoint: { max: 3000, min: 1100 },
+                items: 3,
+              },
+              tablet: {
+                breakpoint: { max: 1100, min: 500 },
+                items: 2,
+              },
+              mobile: {
+                breakpoint: { max: 500, min: 0 },
+                items: 1,
+              },
+            }}
+          >
+            {imagesOfThingsToCarry.map((image) => {
+              return (
+                <div className="things-to-carry-container">
+                  <img
+                    src={image.path}
+                    alt={image.name}
+                    className="things-to-carry-image"
+                  />
+                  <p>{image.name}</p>
+                </div>
+              );
+            })}
+          </Carousel>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return null;
 };
 
 export default ThingsToCarry;
